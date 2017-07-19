61e7a0b352661b7a037e86787ba8295b4d94adc3
codigo <omargonzaleztecheira@gmail.com>
2017-07-19 16:44:16 -0300

    register user

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,Loading,LoadingController, AlertController } from 'ionic-angular';
 
 import { AngularFireAuth } from 'angularfire2/auth';
 import { User } from "../../models/user";
@@ -12,30 +12,61 @@ import { User } from "../../models/user";
 export class Register {
 
   user = {} as User;
+  public loading:Loading;
 
-  constructor(private afAuth:AngularFireAuth,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private afAuth:AngularFireAuth,public navCtrl: NavController,public alertCtrl: AlertController, public navParams: NavParams,public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Register');
   }
   register(){
-      this.afAuth.auth.createUserWithEmailAndPassword(this.user.email,this.user.password).catch(function(error) {
-        // Handle Errors here.
-        //var errorCode = error.code;
-        var errorMessage = error.message;
-        alert(errorMessage);
-        // ...
-        })
-      var user = this.afAuth.auth.currentUser;
-
-    if (user) {
-      console.log("estoy logueado");
-      console.log(user);
-      this.navCtrl.setRoot('Home');
-    } else {
-      console.log("nadie logueado");
-      // No user is signed in.
-      }
+      this.afAuth.auth.createUserWithEmailAndPassword(this.user.email, 
+        this.user.password)
+    .then( authData => {
+      this.loading.dismiss().then( () => {
+        this.navCtrl.setRoot('Home');
+      });
+    }, error => {
+      this.loading.dismiss().then( () => {
+        let alert = this.alertCtrl.create({
+          message: error.message,
+          buttons: [
+            {
+              text: "Ok",
+              role: 'cancel'
+            }
+          ]
+        });
+        alert.present();
+      });
+    });
+    this.loading = this.loadingCtrl.create();
+    this.loading.present();
+    var user = this.afAuth.auth.currentUser;  
+  }
+  loginUser(): void {
+    this.afAuth.auth.signInWithEmailAndPassword(this.user.email, 
+        this.user.password)
+    .then( authData => {
+      this.loading.dismiss().then( () => {
+        this.navCtrl.setRoot('Home');
+      });
+    }, error => {
+      this.loading.dismiss().then( () => {
+        let alert = this.alertCtrl.create({
+          message: error.message,
+          buttons: [
+            {
+              text: "Ok",
+              role: 'cancel'
+            }
+          ]
+        });
+        alert.present();
+      });
+    });
+    this.loading = this.loadingCtrl.create();
+    this.loading.present();
   }
 }
